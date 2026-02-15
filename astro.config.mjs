f22e9f6c49b633a1c12e7f99e098bccee0abc689
manastria <manastria@users.noreply.github.com>
2026-02-15 09:09:00 +0100

    Ajout de nouvelles sections sur le NAT sortant (SNAT) et entrant (DNAT) dans la documentation, ainsi que des modifications mineures pour clarifier les instructions de configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,8 @@ export default defineConfig({
             { label: 'Reset', link: 'activities/sns/reset' },
             { label: 'Configuration de base', link: 'activities/sns/init' },
             { label: 'Modèles pare-feu et NAT', link: 'activities/sns/doc-regles' },
-
+            { label: 'NAT sortant SNAT', link: 'activities/sns/nat-sortant-snat' },
+            { label: 'NAT entrant DNAT', link: 'activities/sns/nat-entrant-dnat' },
           ],
         },
         {
